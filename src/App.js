23a07b78a8fa55948f4148984e25e941d2174d7f
Tate Wongweeratorn <tate16453@gmail.com>
2018-10-22 22:41:22 -0600

    Redo the HomePage and add in Layout and Toolbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './bootstrap-4.1.3-dist/bootstrap.min.css'
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Layout>
         <div className="image">
-          <img src={require('./assets/images/Group 4.png')} class='center'/>
+          <img src={require('./assets/images/Group 4.png')} class='center' alt='campus bar'/>
         </div>
+        </Layout>
       </div>
     );
   }
